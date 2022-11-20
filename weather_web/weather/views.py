d818b64e881f338c551874333fea1ce0723ae5e9
Dhoang-1820 <doh7118@gmail.com>
2022-11-21 00:31:24 +0700

    add router

--- weather_web/weather/views.py
+++ weather_web/weather/views.py
@@ -8,7 +8,6 @@ def index(request):
 
     city_weather = requests.get(url).json()
 
-
     # weather = {
     #     'city': city,
     #     'temperature': city_weather['main']['temp'],
@@ -22,3 +21,23 @@ def index(request):
     print('weather', weather['temperature'])
 
     return render(request, 'index.html', context)  # returns the index.html template
+
+
+def indexCountry(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=d53ae461b2cd691622e8cc940bbdd05b'
+
+    city = 'Ho Chi Minh'
+
+    city_weather = requests.get(url.format(city)).json()
+
+    weather = {
+        'city': city,
+        'temperature': city_weather['main']['temp'],
+        'description': city_weather['weather'][0]['description'],
+        'icon': city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather': weather}
+    print('weather', weather)
+
+    return render(request, 'indexCountry.html', context)  # returns the index.html template
