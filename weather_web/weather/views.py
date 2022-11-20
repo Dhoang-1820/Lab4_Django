2de8cb02b4996e94b61ebdf0b158c87b2031a229
Dhoang-1820 <doh7118@gmail.com>
2022-11-20 23:36:16 +0700

    add view

--- weather_web/weather/views.py
+++ weather_web/weather/views.py
@@ -1,5 +1,24 @@
+import requests
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'weather/index.html') #returns the index.html template
+    url = 'https://lab4-8bee4-default-rtdb.firebaseio.com/test.json'
+
+    city_weather = requests.get(url).json()
+
+
+    # weather = {
+    #     'city': city,
+    #     'temperature': city_weather['main']['temp'],
+    #     'description': city_weather['weather'][0]['description'],
+    #     'icon': city_weather['weather'][0]['icon']
+    # }
+    weather = {
+        'temperature': city_weather['temp'],
+    }
+    context = {'weather': weather}
+    print('weather', weather['temperature'])
+
+    return render(request, 'index.html', context)  # returns the index.html template
